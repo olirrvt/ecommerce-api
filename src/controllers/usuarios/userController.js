2758bed4571e42c791cbaed729f6fbecb504a28a
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2022-12-13 11:20:23 -0300

    building the routes

--- src/controllers/usuarios/userController.js
+++ src/controllers/usuarios/userController.js
@@ -1,5 +1,30 @@
 const userController = (app, db) => {
- 
-}
+
+  app.get("/", (req, res) => {
+    
+      const SQL = `SELECT * FROM users`;
+      new Promise((resolve, reject) => {
+        db.all(SQL, (erro, rows) => {
+            
+            if (!erro) {
+                resolve(rows);
+            } else {
+                reject(erro);
+            };
+
+        });
+      })
+    
+      .then((result) => res.json(result))
+      .catch((error) => console.log(error));
+
+  });
+
+  app.post("/", (req, res) => {});
+
+  app.put("/:id", (req, res) => {});
+
+  app.delete("/:id", (req, res) => {});
+};
 
 export default userController;
