93c9ac817fb589757ac8434389f4631273c0c413
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2022-12-14 11:46:07 -0300

    Construindo as rotas

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,48 +1,95 @@
 import User from "../models/userModels.js";
+import UsersDAO from "../dao/usersDAO.js";
 
 const userController = (app, db) => {
+  const newUsersDao = new UsersDAO(db);
 
-  app.get("/", (req, res) => {
-    
+  // GetAllUsers
+  app.get("/users", async (req, res) => {
+    try {
+      const dataUsers = await newUsersDao.pegarTodosDados();
+      res.json(dataUsers);
+    } catch (erro) {
+      console.log(erro);
+    }
   });
 
-  app.post("/", (req, res) => {
+  // GetUserID
+  app.get('/user/:id', async (req, res) => {
+    try {
+      const id = req.params.id;
+      const dataUser = await newUsersDao.pegarUserId(id);
+      res.status(200).json(dataUser);
+    } catch (erro) {
+      console.log(erro);
+    };
+  });
 
-    const SQL = `INSERT INTO users (id, nome, email, senha) VALUES (?,?,?,?)`;
-
-    const novoUsuario = new User(
-        req.body.nome, 
-        req.body.email, 
+  // PostUser
+  app.post("/newUser", async (req, res) => {
+    try {
+      const novoUsuario = new User(
+        req.body.nome,
+        req.body.email,
         req.body.senha
-    );
+      );
 
-    new Promise((resolve, reject) => {
-      db.all(SQL, 
-        [
-            novoUsuario.id, 
-            novoUsuario.nome, 
-            novoUsuario.email, 
-            novoUsuario.senha
-        ], 
-        (erro) => {
-          
-          if (!erro) {
-              resolve(novoUsuario);
-          } else {
-              reject(erro);
-          };
+      const newUserData = await newUsersDao.InserirDadosNovo(novoUsuario);
+      res
+        .status(200)
+        .json({
+          message: "Usuário inserido com sucesso!",
+          usuario: newUserData,
+        });
+    } catch (erro) {
+      console.log(erro);
+    };
+  });
 
+  // EditUser
+  app.put("/editUser/:id", async (req, res) => {
+
+    try {
+      const id = req.params.id;
+
+      const dadosAntigos = await newUsersDao.pegarUserId(id);
+      console.log(dadosAntigos)
+      // Promise
+
+      const dadosNovos = new User (
+        req.body.nome,
+        req.body.email,
+        req.body.senha
+      );
+
+      const data = await newUsersDao.EditarDados(id, {
+        nome: dadosNovos.nome ? dadosNovos.nome : dadosAntigos.nome,
+        email: dadosNovos.email ? dadosNovos.email : dadosAntigos.email,
+        senha: dadosNovos.senha ? dadosNovos.senha : dadosAntigos.senha
       });
-    })
-  
-    .then((result) => res.json(result))
-    .catch((error) => console.log(error));
+
+      res.status(200).json({
+        message: "Usuário atualizado com sucesso!",
+        usuario: data
+      });
+
+    } catch (erro) {
+      console.log(erro);
+    };
 
   });
 
-  app.put("/:id", (req, res) => {});
+  // DeleteUser
+  app.delete("/user/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const data = await newUsersDao.DeletarDado(id);
+      res.send(data);
+    } catch (erro) {
+      console.log(erro);
+    };
+  });
 
-  app.delete("/:id", (req, res) => {});
 };
 
 export default userController;
--- src/dao/usersDAO.js
+++ src/dao/usersDAO.js
@@ -3,10 +3,11 @@ class UsersDAO {
     this.db = banco;
   }
 
-  getAllDatas() {
+  pegarTodosDados() {
     const SQL = `SELECT * FROM users`;
-    new Promise((resolve, reject) => {
-      db.all(SQL, (erro, rows) => {
+
+    return new Promise((resolve, reject) => {
+      this.db.all(SQL, (erro, rows) => {
         if (!erro) {
           resolve(rows);
         } else {
@@ -16,5 +17,82 @@ class UsersDAO {
     });
   };
 
-  InserirDados() {}
+  pegarUserId(id) {
+
+    const SQL = "SELECT nome, email, senha FROM users WHERE id = ?"
+
+    return new Promise((resolve, reject) => {
+      this.db.all(SQL, id, (erro, rows) => {
+        if (!erro) {
+          resolve(rows)
+        } else {
+          reject(erro)
+        }
+      });
+    });
+
+  };
+  
+  InserirDadosNovo(novoUsuario) {
+    const SQL = `INSERT INTO users (id, nome, email, senha) VALUES (?,?,?,?)`;
+
+    return new Promise((resolve, reject) => {
+      this.db.all(SQL, 
+        [
+            novoUsuario.id, 
+            novoUsuario.nome, 
+            novoUsuario.email, 
+            novoUsuario.senha
+        ], 
+        (erro) => {
+          
+          if (!erro) {
+              resolve(novoUsuario);
+          } else {
+              reject(erro);
+          };
+
+      });
+    });
+  };
+
+  DeletarDado(id){
+
+    const SQL = "DELETE FROM users WHERE id = ?"
+
+    return new Promise((resolve, reject) => {
+      this.db.run(SQL, id, (erro) => {
+        if (!erro) {
+          resolve("Usuário deletado com sucesso!");
+        } else {
+          reject(console.log(erro));
+        };
+      });
+    });
+  };
+
+  EditarDados(id, data){
+
+    const SQL = "UPDATE users SET nome = ?, email = ?, senha = ? WHERE id = ?"
+
+    return new Promise((resolve, reject) => {
+      this.db.run(SQL, id, 
+      this.dadosUsers = [ 
+        data.nome,
+        data.email, 
+        data.senha 
+      ],
+      (erro) => {
+        if (!erro) {
+          resolve(data);
+        } else {
+          reject(console.log(erro));
+        };
+      });
+
+    });
+  };
+
 };
+
+export default UsersDAO;
