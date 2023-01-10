56c3ec74be7efb8be86841409bb47a9e3d8006f7
Taylor Oliveira <tayloroliveira@MBP-de-Taylor.local>
2023-01-10 15:36:35 -0300

    Edit Port and DataBase

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./src/main.js";
-const port = process.env.port | 4200;
+const port = process.env.port | 4000;
 
 app.listen(port, () => {
     console.log(`Service running at the door: http://localhost:${port}`);
